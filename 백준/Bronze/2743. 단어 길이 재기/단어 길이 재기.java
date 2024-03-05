3de3800465a3e48c3bf2d53ab762c68c9343093a
Soyeong <155701627+sy97@users.noreply.github.com>
2024-03-05 12:34:50 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String S = sc.nextLine();
+		System.out.println(S.length());
+				
+	}
+
+}
