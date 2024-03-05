f202307d4b2da0ff59b74459276b67027297d04a
Soyeong <155701627+sy97@users.noreply.github.com>
2024-03-05 12:26:56 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 18688 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		//과목수
+		int N = sc.nextInt();
+		
+		//배열 생성
+		int[] arr = new int[N];
+		
+		//성적 부여
+		for(int i = 0; i < N ; i ++ ) {
+			arr[i] = sc.nextInt();
+		}
+		
+		//최댓값 찾기
+		int max = 0;
+		for(int i = 0; i < N; i++ ) {
+			if(max<arr[i]) {
+				max = arr[i];
+			}
+		}
+		
+		double sum = 0;
+		double avg = 0;
+		for(int i = 0; i < N; i++) {
+			sum += ((double)arr[i]/max)*100;
+		}
+		
+		avg = sum/N;
+		
+		System.out.println(avg);
+	}
+
+}
