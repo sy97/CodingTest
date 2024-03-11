dd96eb8aa554397e9139e0093f3ceae91c5eb980
Soyeong <155701627+sy97@users.noreply.github.com>
2024-03-11 18:21:31 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int input = System.in.read();
+		System.out.println(input);
+		
+	}
+
+}
