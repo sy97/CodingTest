c585032d671dcdcd1f96d1b8503bd50405b7de3f
Soyeong <155701627+sy97@users.noreply.github.com>
2024-03-15 11:32:52 +0900

    [Bronze II] Title: 단어의 개수, Time: 496 ms, Memory: 38496 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		//문자열 입력
+		String str =sc.nextLine().trim();
+		
+		if(str.equals("")) {
+			System.out.println(0);
+		}
+		
+		else {
+		System.out.println(str.split(" ").length);
+		}
+		
+		
+		
+	}
+
+}
